149845f574a5bfb0257195aef3ea88977c5ab8f6
ronok420 <shahariarronok41.com>
2023-12-01 00:01:53 +0600

    conditonal rendering

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart,handleRemoveCart}) => {
+    let message;
+    if(cart.length === 0){
+        message=<p>Add your desire product....!!!! </p>
+        
+    }
     return (
         <div className='single-cart'>
             <h2>Order Summary{cart.length}</h2>
+            {message}
+            {
+            cart.length <= 2  ? 'you can buy more ' : 'congratulation you are the lucky customer' 
+                    
+                
+            }
+            
             {
                cart.map(ts => <p>
-                {ts.name}
+                {ts.name} 
                 <button onClick={()=>handleRemoveCart(ts._id)} style={{marginLeft:'5px'}}>x</button>
                 </p>)
             }
